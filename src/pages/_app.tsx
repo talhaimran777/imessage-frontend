9a4d1a61fea6e0596a94e351a2ae2d03ffc26e5f
Talha Imran <devtal284@gmail.com>
2022-10-29 18:38:12 +0500

    Styled sign in screen

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,24 @@
 import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
-import {
-  ApolloProvider
-} from "@apollo/client";
+import { ApolloProvider } from "@apollo/client";
 import { apolloClient } from "../lib/apollo-client";
+import { ChakraProvider } from "@chakra-ui/react";
 
 interface PROPS {
-  session: Session
+  session: Session;
 }
 
-function MyApp({ Component, pageProps }: AppProps<PROPS >) {
+function MyApp({ Component, pageProps }: AppProps<PROPS>) {
   return (
     <ApolloProvider client={apolloClient}>
       <SessionProvider session={pageProps.session}>
-        <Component {...pageProps} />
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
       </SessionProvider>
     </ApolloProvider>
   );
 }
 
-export default MyApp
+export default MyApp;
--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,28 +1,35 @@
-import type { NextPage } from "next";
+import { Button, Flex, Image, Text } from "@chakra-ui/react";
+import { FcGoogle } from "react-icons/fc";
 import { getProviders, getSession, signIn } from "next-auth/react";
-
-interface Provider {
-  id: string;
-  name: string;
-}
-
-interface SignInProps {
-  providers: [Provider];
-}
+import type { NextPage } from "next";
 
 interface Context {}
 
-const Signin: NextPage<SignInProps> = ({ providers }) => {
+const Signin: NextPage = () => {
   return (
-    <>
-      {Object.values(providers).map((provider) => (
-        <div key={provider.name}>
-          <button onClick={() => signIn("google")}>
-            Sign in with {provider.name}
-          </button>
-        </div>
-      ))}
-    </>
+    <Flex height="100vh" justifyContent="center" alignItems="center">
+      <Flex align="center" direction="column">
+        <Image
+          boxSize="100px"
+          objectFit="cover"
+          src="/images/imessage-logo.svg"
+          alt="Imessage Logo"
+        />
+        <Text fontSize="2xl" mb="3" fontWeight="extrabold">
+          Messenger QL
+        </Text>
+        <Text fontSize="sm" mb="5" align="center">
+          Signin with google to send unlimited free messages to your friends
+        </Text>
+        <Button
+          onClick={() => signIn("google")}
+          backgroundColor="black.400"
+          leftIcon={<FcGoogle />}
+        >
+          Continue with Google
+        </Button>
+      </Flex>
+    </Flex>
   );
 };
 
